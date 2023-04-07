17847696fd442ff821064b8a857e7a24e173cdde
Patrick Lafferty <72819424+pat-lafferty@users.noreply.github.com>
2023-04-07 17:08:55 -0400

    init commit 2

---
+++ terraform/data.tf
@@ -0,0 +1,5 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "this" {
+  name     = var.rg_name
+}
---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = format("%s-%s-%s", var.env, local.common_name, "la")
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "this" {
+  name                = format("%s-%s-%s", var.env, local.common_name, "appi")
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+  workspace_id        = azurerm_log_analytics_workspace.this.id
+  application_type    = "web"
+}
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.this.instrumentation_key
+}
+
+output "app_id" {
+  value = azurerm_application_insights.this.app_id
+}
